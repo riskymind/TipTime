dd342952aefbef69ceb01b8b8aba432930c645fc
kelechi <oparaiskelechi@gmail.com>
2021-02-26 10:33:36 +0100

    done with the activity codes

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        if (stringInTextField == "") {
+            Toast.makeText(this, "what cost?", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val cost: Double = stringInTextField.toDouble()
+
+
+        val selectedTip = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(selectedTip) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        var tip = cost * tipPercentage
+
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        binding.costOfService.setText("")
     }
 }
